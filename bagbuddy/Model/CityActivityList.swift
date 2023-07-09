904ebd686272aa5e9a80640160272adf4d3aa67f
mingshing <mingshing.kuo@foodpanda.tw>
2023-07-10 00:50:11 +0800

    Update the new json to include more information

--- bagbuddy/Model/CityActivityList.swift
+++ bagbuddy/Model/CityActivityList.swift
@@ -10,4 +10,13 @@ import Foundation
 struct CityActivityList: Codable {
     let activities: [Activity]
     let name: String
+    let currency: String
+    let powerAdapter: String
+    let note: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case activities, name, currency, note
+        case powerAdapter = "power_adapter"
+    }
 }
